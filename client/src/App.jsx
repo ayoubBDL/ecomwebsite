d4cff9921dd573b550f860c6b508a50a1cb29d17
ayoubBDL <contact.boudallaa@gmail.com>
2022-08-29 13:32:26 +0100

    test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,12 @@
 import Cart from "./pages/Cart";
 import Home from "./pages/Home";
-import Pay from "./pages/Pay";
 import Login from "./pages/Login";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import {
   BrowserRouter as Router,
-  Switch,
   Route,
-  Link,
   Routes,
   Navigate
 } from "react-router-dom";
@@ -26,7 +23,6 @@ const App = () => {
         <Route exact path="/" element={<Home />} />
         <Route path="/product/:id" element={<Product />} />
         <Route path="/products/:category" element={<ProductList />} />
-        <Route path="/checkout" element={<Pay />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/success" element={<Success />} />
         <Route path="/login" element={user? <Navigate to="/" /> : <Login />} />
